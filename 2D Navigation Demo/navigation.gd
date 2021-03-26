7f50166adf8b147df9a177ec130984e7a46de2cb
aldo hasibuan <SOG-slotracer@gmail.com>
2021-03-26 16:52:23 +0100

    Aldo adds his stuff

--- 2D Navigation Demo/navigation.gd
+++ 2D Navigation Demo/navigation.gd
@@ -1,6 +1,6 @@
 extends Navigation2D
 
-export(float) var character_speed = 30000.0
+export(float) var character_speed = 30000.0 #Pixels / second
 var path = []
 var socket
 var destination
@@ -36,8 +36,9 @@ class car:
 		self._position_track = position_track
 		self._speed = speed
 
-func _process(delta):
-	var walk_distance = character_speed * delta
+func _process(delta): #delta is FPS, which is automaticallly adjusted by the system
+					  #depending on state of the computer at a given time
+	var walk_distance = character_speed * delta #this thus gives pixels/frame
 	move_along_path(walk_distance)
 	var data:String = (socket.get_packet().get_string_from_ascii())
 
@@ -47,15 +48,17 @@ func _process(delta):
 		y = position_array[1]
 		position = Vector2(x, y)
 		_update_navigation_path($Character.position, position) 
+		print("position: ")
 		print(position)
 
 func _exit_tree():
 	thread.wait_to_finish()
 
 func _thread_function(userdata):
-	print("A new thread for communication is created", userdata)
+	print("A new thread for communication is created ", socket)
 	while true:
 		var data:String = (socket.get_packet().get_string_from_ascii())
+		print(data)
 		if data:
 			position_array = data.rsplit(",", true, 6)
 			x = position_array[0]
@@ -68,8 +71,8 @@ func _thread_function(userdata):
 				$GridContainer/ProgressCar1.text = position_array[6]
 
 			_update_navigation_path($Character.position, Vector2(x, y)) 
-			dynamicTrack2DLine.add_point(Vector2(x, y))
-			print(Vector2(x, y))
+			#dynamicTrack2DLine.add_point(Vector2(x, y))
+			
 	
 
 #https://docs.godotengine.org/en/latest/classes/class_packedbytearray.html#class-packedbytearray
@@ -82,10 +85,12 @@ func _input(event):
 	#_update_navigation_path($Character.position, get_local_mouse_position())  # destination
 
 
-func move_along_path(distance):
+func move_along_path(distance): #Distance is pixels required to be traversed in this frame
 	var last_point = $Character.position
-	while path.size():
+	while path.size(): #Path is an array of points that led us to the current position
+		#print(distance)
 		var distance_between_points = last_point.distance_to(path[0])
+		#print(distance_between_points)
 		# The position to move to falls between two points.
 		if distance <= distance_between_points:
 			$Character.position = last_point.linear_interpolate(path[0], distance / distance_between_points)
@@ -94,6 +99,10 @@ func move_along_path(distance):
 		distance -= distance_between_points
 		last_point = path[0]
 		path.remove(0)
+		#socket.put_packet("lol".to_ascii())
+		if Input.is_key_pressed(KEY_SPACE):	
+			print("space")
+			socket.put_packet("1".to_ascii())
 	# The character reached the end of the path.
 	$Character.position = last_point
 	set_process(false)
@@ -104,26 +113,22 @@ func _update_navigation_path(start_position, end_position):
 	# It returns a PoolVector2Array of points that lead you
 	# from the start_position to the end_position.
 	path = get_simple_path(start_position, end_position, true)
+	print(path)
 	# The first point is always the start_position.
 	# We don't need it in this example as it corresponds to the character's position.
 	path.remove(0)
 	set_process(true)
 
-
 func _init():
-	socket = PacketPeerUDP.new()	
+	socket = PacketPeerUDP.new()
 	socket.set_dest_address(server_ip, port)
-	socket.put_packet("Hello I am from Sogeti".to_ascii())
 	socket.put_packet("What are you doing".to_ascii())
+	return
 
 
+			
 func _on_Button_pressed():
 	print("Button Pressed")
-	pass # Replace with function body.
-
-
-func _on_ServerIP_text_entered(new_text):
-	print(new_text)	
 	pass # Replace with function body.
 
 
@@ -138,3 +143,5 @@ func _ready():
 	thread = Thread.new()
 	# Third argument is optional userdata, it can be any variable.
 	thread.start(self, "_thread_function", "Sogeti")
+
+			
