17b24d0cd4457b8f4673386fccac6910cad02c71
STVUGS <stan.vugs@sogeti.com>
2021-05-05 11:42:46 +0200

    refactored godot script

--- 2D Navigation Demo/ServerIP.gd
+++ 2D Navigation Demo/ServerIP.gd
@@ -2,4 +2,4 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text = get_node("/root/Navigation2D").server_ip
+	text = get_node("/root/Navigation2D").SERVER_IP
--- 2D Navigation Demo/navigation.gd
+++ 2D Navigation Demo/navigation.gd
@@ -1,78 +1,78 @@
 extends Navigation2D
 
-export(float) var character_speed = 100000.0 #Pixels / second
 var path = []
-var socket
 var communicator
 var thread
-var server_ip ="127.0.0.1"
-var port = 4242
 var start_tick
 var close_game
 
+var CHARACTER_SPEED = 100000
+var SERVER_IP ="127.0.0.1"
+var PORT = 4242
+
+var exampleJSON: String = '{"eBooks":[{"language":"Pascal","edition":"third"}'\
++ ',{"language":"Python","edition":"four"},{"language":"SQL","edition":"second"}]}'
 
 class Communication:
-	var _server_ip
-	var _port
+	var server_ip
+	var port
 	var socket
 	
 	func _init(server_ip = "127.0.0.1", port=4242):
-		self._server_ip = server_ip
-		self._port = port
+		self.server_ip = server_ip
+		self.port = port
 	
 	func start_communication():
 		self.socket = PacketPeerUDP.new()
-		self.socket.set_dest_address(self._server_ip, self._port)
+		self.socket.set_dest_address(self.server_ip, self.port)
 		self.socket.put_packet("Started socket. Port 4242".to_ascii())
 
 
-class car:
-	var _fuel
-	var _color
-	var _position_track
-	var _speed
+class Car:
+	var name
+	var velocity
+	var derailed
+	var coordinate
+	var race_position
+	var best_lap
+	var last_lap
+	var color
+	
+	func _init(name="car", velocity=0, position_track=0, color='RED'):
+		self.name = name
+		self.velocity = velocity
+		self.race_position = race_position
+		self.color = color
 
-	func _init(fuel=0, color='RED', position_track=0, speed=300):
-		self._fuel = fuel
-		self._color = color
-		self._position_track = position_track
-		self._speed = speed
 
-
-func _physics_process(delta): # delta is 60Hz in _physics_process
-	var walk_distance = character_speed * delta # this gives pixels/frame
+func _physics_process(delta):
+	# delta is 1/60th second
+	# walk distance gives pixels / frame
+	var walk_distance = CHARACTER_SPEED * delta
 	move_along_path(walk_distance)
 	
-	if Input.is_key_pressed(KEY_SPACE):
-		if not start_tick:
-			start_timer()
-		communicator.socket.put_packet("space".to_ascii())
+	send_throttle_on_space()
 	
 	update_timer()
 
 
-func _exit_tree():
-	thread.wait_to_finish()
-
-
-func _thread_function(userdata):
-	print("A new thread for communication is created ", communicator.socket)
+func thread_check_incoming(userdata):
 	while true:
 		if close_game:
-			return
-		var data:String = (communicator.socket.get_packet().get_string_from_ascii())
+			return # close thread upon closing of game
+		var data = get_data()
 		if data:
 			print(data)
 			
-			# var dictionary: Dictionary = JSON.parse(data).result
-			# print(dictionary)
+			var dictionary: Dictionary = JSON.parse(exampleJSON).result
+			print(dictionary["eBooks"][0]["edition"])
 			# update_car_classes(dictionary)
 			
 			if data == "derailed":
 				#If cart is derailed, game stops
 				$Derailed.visible = true
 				print("derailed!")
-				start_tick = 0
+				reset_timer()
 			else:
 				var position_array = data.rsplit(",", true, 6)
 				var x = position_array[0]
@@ -83,15 +83,14 @@ func _thread_function(userdata):
 					$GridContainer/LastLapCar1.text = position_array[4]
 					$GridContainer/BestLapCar1.text = position_array[5]
 					$GridContainer/ProgressCar1.text = position_array[6]
-				_update_navigation_path($Car2.position, Vector2(x, y))
+				update_navigation_path($Car2.position, Vector2(x, y))
 
 
 func _input(event):
-	# The 'click' event is a custom input action defined in
-	# Project > Project Settings > Input Map tab.
+	# events are defined in: Project > Project Settings > Input Map tab
 	if event.is_action_pressed("r"):
 		$Derailed.visible = false
-		start_tick = 0
+		reset_timer()
 		communicator.socket.put_packet("reset".to_ascii())
 	
 	if event.is_action_pressed("esc"):
@@ -100,7 +99,20 @@ func _input(event):
 		get_tree().quit()
 
 
+func send_throttle_on_space():
+	if Input.is_key_pressed(KEY_SPACE):
+		if not start_tick:
+			# only start timer if not yet started
+			start_timer()
+		communicator.socket.put_packet("space".to_ascii())
+
+
+func get_data():
+	return communicator.socket.get_packet().get_string_from_ascii()
+
+
 func update_car_classes(data):
+	# car classes should be updated with data in JSON format
 	return data
 
 
@@ -109,13 +121,16 @@ func start_timer():
 
 
 func update_timer():
-	if start_tick:
+	if start_tick: # only start showing the time if the timer started
 		var current_time = OS.get_ticks_msec() - start_tick
-		current_time = format_time(current_time)
-		$Control/Legend2/PanelContainer/Timerlabel.text = current_time
+		$Control/Legend2/PanelContainer/Timerlabel.text = ms_To_mm_ss_msmsms(current_time)
 
 
-func format_time(time):
+func reset_timer():
+	start_tick = 0
+
+
+func ms_To_mm_ss_msmsms(time):
 	var digits = []
 	var minutes = "%02d" % [time / 60000]
 	digits.append(minutes)
@@ -132,9 +147,9 @@ func format_time(time):
 	return formatted
 
 
-func move_along_path(distance): #Distance is pixels required to be traversed in this frame
+func move_along_path(distance): # Distance is pixels required to be traversed in this frame
 	var last_point = $Car2.position
-	while path.size(): #Path is an array of points that led us to the current position
+	while path.size(): # Path is an array of points that led us to the current position
 		var distance_between_points = last_point.distance_to(path[0])
 		# The position to move to falls between two points.
 		if distance <= distance_between_points:
@@ -149,9 +164,8 @@ func move_along_path(distance): #Distance is pixels required to be traversed in 
 	set_physics_process(false)
 
 
-func _update_navigation_path(start_position, end_position):
-	# get_simple_path is part of the Navigation2D class.
-	# It returns a PoolVector2Array of points that lead you
+func update_navigation_path(start_position, end_position):
+	# get_simple_path returns a PoolVector2Array of points that lead you
 	# from the start_position to the end_position.
 	path = get_simple_path(start_position, end_position, true)
 	# The first point is always the start_position.
@@ -161,16 +175,9 @@ func _update_navigation_path(start_position, end_position):
 
 
 func _init():
-	communicator = Communication.new("127.0.0.1",4242)
+	communicator = Communication.new(SERVER_IP, PORT)
 	communicator.start_communication()
-	return
-	# socket = PacketPeerUDP.new()
-	# socket.set_dest_address(server_ip, port)
-	# socket.put_packet("Started socket. Port 4242".to_ascii())
-
-
-func _on_Button_pressed():
-	pass # Replace with function body.
+	print("A new thread for communication is created ", communicator.socket)
 
 
 func _on_Port_text_entered(new_text):
@@ -180,5 +187,6 @@ func _on_Port_text_entered(new_text):
 func _ready():
 	thread = Thread.new()
 	# Third argument is optional userdata, it can be any variable.
-	thread.start(self, "_thread_function", "sogeti")
+	# Thread is used to asynchronously check incoming data
+	thread.start(self, "thread_check_incoming", "sogeti")
 
